8d961df03469c0dcc1961f7f2e83e84cfe326039
Scott Morrison <scottmorr75@gmail.com>
2020-06-08 19:38:16 -0600

    first

---
+++ routes/html-routes.js
@@ -0,0 +1,66 @@
+// Requiring path to so we can use relative routes to our HTML files
+const path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+const isAuthenticated = require("../config/middleware/isAuthenticated");
+
+const recipes = [
+  {
+    id: 1,
+    name: "Pasta",
+    ingredients: ["noodles", "tomato sauce"]
+  },
+  {
+    id: 2,
+    name: "Steak",
+    ingredients: ["steak", "salt", "pepper"]
+  }
+];
+
+module.exports = function(app) {
+  app.get("/", (req, res) => {
+    res.render("index");
+  });
+
+  app.get("/shopping", (req, res) => {
+    res.render("shopping");
+  });
+
+  app.get("/recipes", (req, res) => {
+    res.render("recipes", {
+      title: "My recipes!",
+      recipes
+    });
+  });
+
+  app.get("/recipe/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const recipe = recipes.find(x => x.id === id);
+
+    res.render("recipe", {
+      recipe
+    });
+  });
+
+  // app.get("/", (req, res) => {
+  //   // If the user already has an account send them to the members page
+  //   if (req.user) {
+  //     return res.redirect("/members");
+  //   }
+  //   res.sendFile(path.join(__dirname, "../public/signup.html"));
+  // });
+
+  // app.get("/login", (req, res) => {
+  //   // If the user already has an account send them to the members page
+  //   if (req.user) {
+  //     return res.redirect("/members");
+  //   }
+  //   res.sendFile(path.join(__dirname, "../public/login.html"));
+  // });
+
+  // // Here we've add our isAuthenticated middleware to this route.
+  // // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  // app.get("/members", isAuthenticated, (req, res) => {
+  //   res.sendFile(path.join(__dirname, "../public/members.html"));
+  // });
+};
---
+++ server.js
@@ -0,0 +1,51 @@
+// Requiring necessary npm packages
+const express = require("express");
+const session = require("express-session"); //what does this do?
+// Requiring passport as we've configured it
+const passport = require("./config/passport"); //for user authentication
+
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
+
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+app.use(express.urlencoded({ extended: true })); //configuration middleware from express - response will be in json form
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true }) //persistent login status when navigating urls within the webpage
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+const hbs = require("express-handlebars");
+app.engine("handlebars", hbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+const routes = require("./controllers/pantreeController.js");
+
+app.use(routes);
+
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
