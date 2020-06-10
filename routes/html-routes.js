a2152c3b14975a1790c1699fa7b8d155532a3255
Scott Morrison <scottmorr75@gmail.com>
2020-06-10 17:53:34 -0600

    fixed branch

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,12 +5,10 @@ const fs = require("fs");
 // Requiring our custom middleware for checking if a user is logged in
 // const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-
 module.exports = function(app) {
   app.get("/", (req, res) => {
     res.render("index");
   });
-
 
   app.get("/rps", (req, res) => {
     res.render("rps");
@@ -34,53 +32,51 @@ module.exports = function(app) {
   app.get("/maincourse", (req, res) => {
     res.render("maincourse");
 
-  app.get("/login", function(req, res) {
-    // If the user already has an account send them to the front page
-    if (req.user) {
-      res.redirect("/");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
-
-  app.get("/createAccount", (req, res) => {
-    if(req.user) {
-      res.redirect("/");
-    }
-    else {
-      res.sendFile(path.join(__dirname, "../public/createAccount.html"));
-    }
-  });
-
-  app.get("/shopping", (req, res) => {
-    if(!req.user) {
-      res.redirect("/login");
-    }
-    else {
-      res.render("shopping");
-    }
-
-  });
-
-  app.get("/recipes", (req, res) => {
-    fs.readFile(__dirname + "/../db/db.json", "utf8", (err, data) => {
-      if (err) {
-        console.log(err);
+    app.get("/login", (req, res) => {
+      // If the user already has an account send them to the front page
+      if (req.user) {
+        res.redirect("/");
       }
-      // parse it so that it is an array
-      const recipes = JSON.parse(data);
-      res.render("rps", {
-        title: "My recipes!",
-        rps: recipes
+      res.sendFile(path.join(__dirname, "../public/login.html"));
+    });
+
+    app.get("/createAccount", (req, res) => {
+      if (req.user) {
+        res.redirect("/");
+      } else {
+        res.sendFile(path.join(__dirname, "../public/createAccount.html"));
+      }
+    });
+
+    app.get("/shopping", (req, res) => {
+      if (!req.user) {
+        res.redirect("/login");
+      } else {
+        res.render("shopping");
+      }
+    });
+
+    app.get("/recipes", (req, res) => {
+      fs.readFile(__dirname + "/../db/db.json", "utf8", (err, data) => {
+        if (err) {
+          console.log(err);
+        }
+        // parse it so that it is an array
+        const recipes = JSON.parse(data);
+        res.render("rps", {
+          title: "My recipes!",
+          rps: recipes
+        });
       });
     });
-  });
 
-  app.get("/recipe/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const recipe = recipes.find(x => x.id === id);
+    app.get("/recipe/:id", (req, res) => {
+      const id = parseInt(req.params.id);
+      const recipe = recipes.find(x => x.id === id);
 
-    res.render("recipe", {
-      recipe
+      res.render("recipe", {
+        recipe
+      });
     });
   });
 };
