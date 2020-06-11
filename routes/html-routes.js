96be0ca8fa91fe0ce9632a3881db50e53ca69320
Brent Peterson <brentp24@gmail.com>
2020-06-10 22:43:08 -0600

    handlebars for rps

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,9 +2,6 @@
 const path = require("path");
 const fs = require("fs");
 
-// Requiring our custom middleware for checking if a user is logged in
-const isAuthenticated = require("../config/middleware/isAuthenticated");
-
 module.exports = function(app) {
   app.get("/", (req, res) => {
     res.render("index");
@@ -26,7 +23,6 @@ module.exports = function(app) {
       // parse it so that it is an array
       const recipes = JSON.parse(data);
       res.render("recipe", {
-        title: "My recipes!",
         rps: recipes
       });
     });
@@ -98,21 +94,19 @@ module.exports = function(app) {
     }
   });
 
-
-    app.get("/recipes", (req, res) => {
-      fs.readFile(__dirname + "/../db/db.json", "utf8", (err, data) => {
-        if (err) {
-          console.log(err);
-        }
-        // parse it so that it is an array
-        const recipes = JSON.parse(data);
-        res.render("rps", {
-          title: "My recipes!",
-          rps: recipes
-        });
+  app.get("/recipes", (req, res) => {
+    fs.readFile(__dirname + "/../db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      // parse it so that it is an array
+      const recipes = JSON.parse(data);
+      res.render("rps", {
+        title: "My recipes!",
+        rps: recipes
       });
     });
-  
+  });
 
   app.get("/recipe/:id", (req, res) => {
     const id = parseInt(req.params.id);
@@ -126,5 +120,4 @@ module.exports = function(app) {
   app.get("/addrecipes", (req, res) => {
     res.render("addrecipes");
   });
-
 };
